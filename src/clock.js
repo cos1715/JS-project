26b9ee254838a7f90455a607b44a6ca70c36c3ea
Taras <tsamoilenko97@gmail.com>
2018-11-01 19:31:40 +0200

    solved linter errors

--- src/clock.js
+++ src/clock.js
@@ -4,16 +4,16 @@ const $clock = document.getElementById('clock-display');
 const $date = document.getElementById('date-display');
 
 const showDate = ($el, options) => {
-    const date = new Date();
-    $el.textContent = date.toLocaleString('uk-UA', options);
-}
+  const date = new Date();
+  $el.textContent = date.toLocaleString('uk-UA', options);
+};
 
 const initClock = () => {
-    const clockOptions = { hour: 'numeric', minute: 'numeric' };
-    const dateOptions = { year: 'numeric', month: 'numeric', day: 'numeric' };
-    showDate($clock, clockOptions);
-    showDate($date, dateOptions);
-    setInterval(() => showDate($clock, clockOptions), 1000);
-}
+  const clockOptions = { hour: 'numeric', minute: 'numeric' };
+  const dateOptions = { year: 'numeric', month: 'numeric', day: 'numeric' };
+  showDate($clock, clockOptions);
+  showDate($date, dateOptions);
+  setInterval(() => showDate($clock, clockOptions), 1000);
+};
 
 initClock();
--- src/main.js
+++ src/main.js
@@ -7,78 +7,77 @@ const todoArr = [];
 
 
 const addClass = (element, className) => {
-    element.classList.add(className);
-}
+  element.classList.add(className);
+};
 
 const removeClass = (element, className) => {
-    element.classList.remove(className);
-}
+  element.classList.remove(className);
+};
 
 const listSectionClassToggle = (element, className) => {
-    const classList = Array.from($todoListSection.classList);
+  const classList = Array.from($todoListSection.classList);
 
-    if (classList.includes('main-section-focus')) {
-        element.classList.add(className);
+  if (classList.includes('main-section-focus')) {
+    element.classList.add(className);
+  } else {
+    if (!($todoList.childNodes.length > 1)) {
+      element.classList.remove(className);
     }
-    else {
-        if (!($todoList.childNodes.length > 1)) {
-            element.classList.remove(className);
-        }
-    }
-}
+  }
+};
 
 const createTodoElement = content => {
-    const $li = document.createElement('li');
+  const $li = document.createElement('li');
 
-    $li.className = 'section-ul-li';
-    $li.innerHTML = `
+  $li.className = 'section-ul-li';
+  $li.innerHTML = `
     <label class="list-item-label">
         <input type="checkbox" class="list-item-checkbox" />
         <span class="list-item-text">${content}</span>
         <span class="list-item-checkmark"></span>
     </label>
     <i class="fas fa-times list-item-remove-icon"></i>`;
-    return $li;
+  return $li;
 };
 
 const createTag = ({ key, target }) => {
-    if (key === 'Enter' && target.value.trim()) {
-        const $el = createTodoElement(target.value);
-        todoArr.push(target.value);
-        const height = 45 * todoArr.length;
-        $todoList.style.height = `${height}px`;
-        $todoList.appendChild($el);
-        $el.classList.add('opacity-to-one');
-        $todoInput.value = '';
+  if (key === 'Enter' && target.value.trim()) {
+    const $el = createTodoElement(target.value);
+    todoArr.push(target.value);
+    const height = 45 * todoArr.length;
+    $todoList.style.height = `${height}px`;
+    $todoList.appendChild($el);
+    $el.classList.add('opacity-to-one');
+    $todoInput.value = '';
 
-    }
-}
+  }
+};
 
-const removeTag = (event) => {
-    const { target } = event;
-    const isRemove = target.classList.contains('list-item-remove-icon');
-    const $todo = target.parentElement;
-    const text = $todo.getElementsByClassName('list-item-text')[0].innerHTML;
+const removeTag = event => {
+  const { target } = event;
+  const isRemove = target.classList.contains('list-item-remove-icon');
+  const $todo = target.parentElement;
+  const text = $todo.getElementsByClassName('list-item-text')[0].innerHTML;
 
-    if (isRemove) {
-        $todoList.removeChild($todo);
-        const index = todoArr.indexOf(text);
-        todoArr.splice(index, 1);
-        const height = 45 * todoArr.length;
-        $todoList.style.height = `${height}px`;
-        listSectionClassToggle($todoListSection, 'main-section-active');
-    }
-}
+  if (isRemove) {
+    $todoList.removeChild($todo);
+    const index = todoArr.indexOf(text);
+    todoArr.splice(index, 1);
+    const height = 45 * todoArr.length;
+    $todoList.style.height = `${height}px`;
+    listSectionClassToggle($todoListSection, 'main-section-active');
+  }
+};
 
 $todoInput.addEventListener('focus',
-    () => addClass($todoListSection, 'main-section-focus'));
+  () => addClass($todoListSection, 'main-section-focus'));
 $todoInput.addEventListener('blur',
-    () => removeClass($todoListSection, 'main-section-focus'));
+  () => removeClass($todoListSection, 'main-section-focus'));
 $todoInput.addEventListener('keyup',
-    () => createTag(event));
+  () => createTag(event));
 $todoListSection.addEventListener('transitionend',
-    () => listSectionClassToggle($todoListSection, 'main-section-active'));
+  () => listSectionClassToggle($todoListSection, 'main-section-active'));
 $todoList.addEventListener('click',
-    () => removeTag(event));
+  () => removeTag(event));
 
 
