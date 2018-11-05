d06cd6627252d72dc83e6dda43a3bb1e1036e74b
Taras <tsamoilenko97@gmail.com>
2018-11-05 21:40:58 +0200

    added weather

--- readme.md
+++ readme.md
@@ -3,20 +3,24 @@
 * Course curator - [Volodymyr Shevchuk](https://github.com/dosandk)
 * Front-camp [facebook](https://www.facebook.com/groups/270300106928894)
 
-## Name of your project
+## Todo-app
 
-[Demo]()  
-> Description - your project description  
+[Demo](https://cos1715.github.io/Todo-app/)  
+> Description: "To do list" - a program which helps to create your daily list of tasks.
+
+> Main idea: This app was created to organize your time and boost your productivity.
+
+> For whom: "To do list" was created for busy and mind worthy persons who have a need in simple and useful program for writing their daily tasks.  
 
 ## Tech stack
 
 * javascript 
 * html/css
-* ...
+* scss
 
 ## Mockups
 
-[Link]()  
+[wireframe](https://wireframepro.mockflow.com/view/M50616f85faf2817ae5c20f9aad6ecaec1539521806936#/page/c40699da9a6946f49538df3a59c8422b)  
 
 ## Contribution guide
 
--- src/clock.js
+++ src/clock.js
@@ -1,19 +1,47 @@
 'use strict';
-
 const $clock = document.getElementById('clock-display');
 const $date = document.getElementById('date-display');
+const $weather = document.getElementById('weather-display');
 
-const showDate = ($el, options) => {
-  const date = new Date();
-  $el.textContent = date.toLocaleString('uk-UA', options);
-};
+class App {
+  constructor ($clock, $date, $weather) {
+    this.$clock = $clock;
+    this.$date = $date;
+    this.$weather = $weather;
+    this.timeFormat = 'uk-UA';
+    this.clockOptions = { hour: 'numeric', minute: 'numeric' };
+    this.dateOptions = { year: 'numeric', month: 'numeric', day: 'numeric' };
 
-const initClock = () => {
-  const clockOptions = { hour: 'numeric', minute: 'numeric' };
-  const dateOptions = { year: 'numeric', month: 'numeric', day: 'numeric' };
-  showDate($clock, clockOptions);
-  showDate($date, dateOptions);
-  setInterval(() => showDate($clock, clockOptions), 1000);
-};
+    this.showDate(this.$clock, this.timeFormat, this.clockOptions);
+    this.showDate(this.$date, this.timeFormat, this.dateOptions);
+    navigator.geolocation.getCurrentPosition(position => this.getWeather(position, this.$weather));
+  }
+  showDate ($el, timeFormat, options) {
+    const date = new Date();
 
-initClock();
+    $el.textContent = date.toLocaleString(timeFormat, options);
+  }
+  runClock () {
+    const timeInterval = 1000;
+
+    setInterval(() =>
+      this.showDate(this.$clock, this.timeFormat, this.clockOptions), timeInterval);
+  }
+  async getWeather (position, $el) {
+    const crd = position.coords;
+    const lat = crd.latitude;
+    const long = crd.longitude;
+    const response = await fetch(`https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20
+    weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20
+    geo.places(1)%20where%20text%3D"(${lat},${long})")and%20
+    u%3D%27c%27&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`);
+    const data = await response.json();
+
+    $el.textContent = `${data.query.results.channel.location.city} 
+    ${data.query.results.channel.item.condition.temp}°C`;
+  }
+}
+
+const app = new App($clock, $date, $weather);
+
+app.runClock();
