a10fd86e7ab000d07101bf8f648d40d0a6bf5c99
Taras <tsamoilenko97@gmail.com>
2018-11-08 19:09:36 +0200

    sw code update

--- sw.js
+++ sw.js
@@ -1,62 +1,64 @@
-this.addEventListener('install', e => {
+const cacheName = 'todo-app';
+
+const cacheFiles = [
+  './',
+  './index.html',
+  './src/scss/components/aside/aside.css',
+  './src/scss/components/checkbox/checkbox.css',
+  './src/scss/components/header/header.css',
+  './src/scss/components/main/main.css',
+  './src/date.js',
+  './src/list.js',
+  './src/image/browser-icon.png',
+  'https://fonts.googleapis.com/css?family=Lato|Roboto'
+];
+
+
+self.addEventListener('install', function (e) {
   e.waitUntil(
-    caches.open('todo-app').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './src/scss/components/aside/aside.css',
-        './src/scss/components/checkbox/checkbox.css',
-        './src/scss/components/header/header.css',
-        './src/scss/components/main/main.css',
-        './src/date.js',
-        './src/list.js',
-        './src/image/browser-icon.png',
-        'https://fonts.googleapis.com/css?family=Lato|Roboto'
-      ]);
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(cacheFiles);
     })
   );
 });
 
-this.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(() => {
-    return fetch(event.request);
-  }).then(response => {
-    caches.open('todo-app').then(cache => {
-      cache.put(event.request, response);
-    });
-    return response.clone();
-  }));
-});
 
-this.addEventListener('install', e => {
+self.addEventListener('activate', function (e) {
   e.waitUntil(
-    caches.open('todo-app1').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './src/scss/components/aside/aside.css',
-        './src/scss/components/checkbox/checkbox.css',
-        './src/scss/components/header/header.css',
-        './src/scss/components/main/main.css',
-        './src/date.js',
-        './src/list.js',
-        './src/image/browser-icon.png',
-        'https://fonts.googleapis.com/css?family=Lato|Roboto'
-      ]);
-    })
-  );
-});
-
-this.addEventListener('activate', event => {
-  const cacheWhitelist = ['todo-app1'];
-
-  event.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(keyList.map(key => {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          return caches.delete(key);
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(cacheNames.map(function (thisCacheName) {
+        if (thisCacheName !== cacheName) {
+          return caches.delete(thisCacheName);
         }
       }));
     })
+  );
+
+});
+
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        const requestClone = e.request.clone();
+
+        fetch(requestClone)
+          .then(function (response) {
+
+            if (!response) {
+              return response;
+            }
+            const responseClone = response.clone();
+
+            caches.open(cacheName).then(function (cache) {
+              cache.put(e.request, responseClone);
+              return response;
+            });
+          });
+      })
   );
 });
