fa1c18df33d1861d4b6f48f73f2a89c095a702f8
Taras <tsamoilenko97@gmail.com>
2018-11-16 17:45:11 +0200

    solved 302 issue

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const cacheName = 'todo-app';
-const cacheFiles = [
+const CACHE_VERSION = 'v1';
+const RESOURCES = [
   '/Todo-app/',
   '/Todo-app/index.html',
   '/Todo-app/src/scss/components/index.css',
@@ -9,43 +9,56 @@ const cacheFiles = [
   'https://fonts.googleapis.com/css?family=Lato|Fredoka+One'
 ];
 
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(cacheFiles);
-    })
-  );
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.open(cacheName)
-      .then(cache => cache.keys())
-      .then(keys => deleteCache(keys))
-      .then(() => self.clients.claim())
-  );
-});
-
-const deleteCache = names => {
-  return Promise.all(
-    names
-      .filter(cacheName => cacheName !== cacheName)
-      .map(cacheName => caches.delete(cacheName))
-  );
+const logger = {
+  success (...args) {
+    console.log(`%c${args.join(' ')}`, 'color: green');
+  }
 };
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.open(cacheName).then(cache => {
-      return cache.match(event.request).then(response => {
-        return response || fetch(event.request)
-          .then(response => {
-            const responseClone = response.clone();
-            cache.put(event.request, responseClone);
-          });
-      });
-    }
-    )
-  );
-});
+const sw = {
+  initialize () {
+    logger.success('initialize');
+    self.addEventListener('install', sw.onInstall);
+    self.addEventListener('activate', sw.onActivate);
+    self.addEventListener('fetch', sw.onFetch);
+  },
+  onInstall (event) {
+    logger.success('install');
+
+    event.waitUntil(
+      caches.open(CACHE_VERSION).then(cache => cache.addAll(RESOURCES))
+    );
+  },
+  onActivate (event) {
+    logger.success('activate');
+
+    event.waitUntil(
+      caches.open(CACHE_VERSION)
+        .then(cache => cache.keys())
+        .then(keys => sw.deleteCache(keys))
+        .then(() => self.clients.claim())
+    );
+  },
+  deleteCache (names) {
+    return Promise.all(
+      names
+        .filter(cacheName => cacheName !== CACHE_VERSION)
+        .map(cacheName => caches.delete(cacheName))
+    );
+  },
+  onFetch (event) {
+    event.respondWith(
+      caches.open(CACHE_VERSION).then(cache => {
+        return cache.match(event.request).then(response => {
+          return response || fetch(event.request)
+            .then(response => {
+              const responseClone = response.clone();
+              cache.put(event.request, responseClone);
+            });
+        });
+      }
+      ));
+  }
+};
+
+sw.initialize();
